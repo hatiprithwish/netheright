6d4c40e3db07301b75d76d076930f8446b3d4148
Prithwish Hati <hatiprithwish@gmail.com>
2026-03-10 08:06:27 +0530

    feat: added terms and privacy pages

--- frontend/ui/common/utils.ts
+++ frontend/ui/common/utils.ts
@@ -2,13 +2,16 @@ export const HeaderLinks = [
   {
     label: "Dashboard",
     href: "/dashboard",
+    isProtected: true,
   },
   {
     label: "Problems",
     href: "/problems",
+    isProtected: false,
   },
   {
     label: "Architecture",
     href: "/architecture",
+    isProtected: false,
   },
 ];
